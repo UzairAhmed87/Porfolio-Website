d4399445d39d33145bfdba5960cf4d4c21618721
UzairAhmed87 <uzairahmed1574@gmail.com>
2024-06-09 18:25:03 +0500

    modified

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -2,23 +2,47 @@ const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
 const styleSwitcher = document.querySelector(".style-switcher");
 const alternateStyle = document.querySelectorAll(".alternate-style");
 const dayNight = document.querySelector(".day-night");
+let currentStyle = 0;
 
-styleSwitcherToggle.addEventListener("click", () => {
-  styleSwitcher.setAttribute("style", "transform:translateX(-25px);");
+window.addEventListener("load", () => {
+  function changeStyle() {
+    // Disable all styles
+    alternateStyle.forEach((style) => {
+      style.disabled = true;
+    });
+    // Enable the next style
+    alternateStyle[currentStyle].disabled = false;
+    // Move to the next style, looping back to the first style if necessary
+    currentStyle = (currentStyle + 1) % alternateStyle.length;
+  }
+
+  // Set an interval to change styles every 2 seconds
+  setInterval(changeStyle, 1000);
+  // Initial style change
+  changeStyle();
   
 });
 
-window.addEventListener("scroll", () => {
-  styleSwitcher.setAttribute("style", "transform:translateX(100%);");
+styleSwitcher.addEventListener("click", () => {
+  if (styleSwitcher.classList.contains("open")) {
+    styleSwitcher.classList.remove("open");
+   
+  } else {
+    styleSwitcher.classList.add("open");
+  }
 });
 
 function setActiveStyle(color) {
   alternateStyle.forEach((style) => {
     if (color === style.getAttribute("title")) {
       style.removeAttribute("disabled");
+    
+      
     } else {
       style.setAttribute("disabled", "true");
+     
     }
+    
   });
 }
 
